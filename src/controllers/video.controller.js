308048d47a884f410d129882a0ec28a4a8bb2781
ankit <ankit21014@iiitd.ac.in>
2024-06-08 19:58:43 +0530

    publish fixed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -140,7 +140,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
             public_id: thumbnail.public_id
         },
         owner: req.user?._id,
-        isPublished: false
+        isPublished: true
     });
 
     const videoUploaded = await Video.findById(video._id);
